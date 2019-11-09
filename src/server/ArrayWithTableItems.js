3d0704f6950d55c62a46e98bc2bd104c4b2b3af2
sasha53545 <96.kazzak.96@gmail.com>
2019-11-09 10:17:11 +0300

    Change structure of project

--- src/server/ArrayWithTableItems.js
+++ src/server/ArrayWithTableItems.js
@@ -3,10 +3,7 @@ import React from "react";
 let i = 1;
 
 const serverDataArray = [
-    {indexEl: i++, name: "Moscowbank", bik: "123", kor: "121233", address: "Moscow"},
-    {indexEl: i++, name: "Murmankbank", bik: "413", kor: "16223", address: "Murmansk"},
-    {indexEl: i++, name: "Rostovbank", bik: "16243", kor: "113423", address: "Rostov"},
-    {indexEl: i++, name: "Kalugabank", bik: "1143", kor: "162423", address: "Kaluga"},
+
 ];
 
 export default serverDataArray;
